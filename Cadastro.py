3c671f759023d387d36abd00345f031703e2a062
Motorespka <k45430494@gmail.com>
2026-03-25 23:16:54 -0300

    Update Cadastro.py

--- Cadastro.py
+++ Cadastro.py
@@ -97,7 +97,7 @@ def show():
             else:
                 st.warning("Marca e Modelo são obrigatórios.")
 
-dados_ocr = ler_placa_motor(caminho_imagem)
+dados_ocr = ler_placa_motor(caminho_temp)
 st.sucess("Dados encontrados!")
 st.write(dados_ocr)
 
