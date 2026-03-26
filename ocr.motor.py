b928a0c928b4ca102668b744df334db6f5edebb5
Motorespka <k45430494@gmail.com>
2026-03-25 22:55:35 -0300

    Create ocr.motor.py

---
+++ ocr.motor.py
@@ -0,0 +1,29 @@
+import easyocr
+
+# inicia leitor (português + inglês)
+reader = easyocr.Reader(['pt','en'])
+
+def ler_placa_motor(caminho_imagem):
+
+    resultado = reader.readtext(caminho_imagem)
+
+    texto_total = " ".join([r[1] for r in resultado])
+
+    dados = {}
+
+    # EXTRAÇÕES SIMPLES (vamos melhorar depois)
+    if "WEG" in texto_total.upper():
+        dados["marca"] = "WEG"
+
+    if "220" in texto_total:
+        dados["tensao"] = 220
+
+    if "380" in texto_total:
+        dados["tensao"] = 380
+
+    if "4" in texto_total:
+        dados["polos"] = 4
+
+    dados["texto_detectado"] = texto_total
+
+    return dados
