f0070778e871aa100dbd728f11bdf4f1baf73d87
Motorespka <k45430494@gmail.com>
2026-03-27 18:15:59 -0300

    Update login.py

--- auth/login.py
+++ auth/login.py
@@ -1,1 +1,16 @@
+import streamlit as st
 
+def check_login():
+
+    if "logado" not in st.session_state:
+        st.session_state.logado = False
+
+    if not st.session_state.logado:
+
+        senha = st.text_input("Senha", type="password")
+
+        if senha == "1234":
+            st.session_state.logado = True
+            st.rerun()
+
+        st.stop()
