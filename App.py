ca3a0b2fdcc1e1db8b212f9ccb9a035630c66ea5
Motorespka <k45430494@gmail.com>
2026-03-27 15:35:17 -0300

    Update App.py

--- App.py
+++ App.py
@@ -1,13 +1,40 @@
 import streamlit as st
-from auth.login import check_login
 
+# ---------------- CONFIG ----------------
 st.set_page_config(
     page_title="Moto-Renow",
     layout="wide"
 )
 
+# ---------------- LOGIN ----------------
+def check_login():
+
+    if "logado" not in st.session_state:
+        st.session_state.logado = False
+
+    if not st.session_state.logado:
+
+        st.title("🔐 Login Moto-Renow")
+
+        usuario = st.text_input("Usuário")
+        senha = st.text_input("Senha", type="password")
+
+        if st.button("Entrar"):
+
+            if (
+                usuario == st.secrets["login"]["usuario"]
+                and senha == st.secrets["login"]["senha"]
+            ):
+                st.session_state.logado = True
+                st.rerun()
+            else:
+                st.error("Usuário ou senha incorretos")
+
+        st.stop()
+
 check_login()
 
+# ---------------- APP ----------------
 st.title("Moto-Renow")
 
 menu = st.sidebar.selectbox(
