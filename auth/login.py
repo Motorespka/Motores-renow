640e910a6611c491c4c45e94713b94f6ce1c312e
Motorespka <k45430494@gmail.com>
2026-03-30 11:13:25 -0300

    Update login.py

--- auth/login.py
+++ auth/login.py
@@ -2,15 +2,33 @@ import streamlit as st
 
 def check_login():
 
+    # cria variável de sessão
     if "logado" not in st.session_state:
         st.session_state.logado = False
 
-    if not st.session_state.logado:
+    # se já estiver logado
+    if st.session_state.logado:
+        return
 
-        senha = st.text_input("Senha", type="password")
+    st.title("🔐 Moto-Renow - Acesso Técnico")
 
-        if senha == "1234":
-            st.session_state.logado = True
-            st.rerun()
+    senha = st.text_input(
+        "Digite a chave técnica",
+        type="password",
+        key="senha_login"
+    )
 
-        st.stop()
+    if st.button("Entrar"):
+
+        try:
+            if senha == st.secrets["APP_PASSWORD"]:
+                st.session_state.logado = True
+                st.success("Login realizado")
+                st.rerun()
+            else:
+                st.error("Senha incorreta")
+
+        except Exception:
+            st.error("APP_PASSWORD não configurada no secrets")
+
+    st.stop()
