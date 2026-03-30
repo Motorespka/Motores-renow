c37d7060d35457ac0b21d2bfd361dfe0c198e3ec
Motorespka <k45430494@gmail.com>
2026-03-30 11:48:45 -0300

    Update App.py

--- App.py
+++ App.py
@@ -1,71 +1,61 @@
 import streamlit as st
 import importlib
 
-# ===============================
+# ==========================
 # CONFIG
-# ===============================
+# ==========================
 st.set_page_config(
     page_title="Moto-Renow",
     page_icon="⚙️",
     layout="wide"
 )
 
-# ===============================
-# LOGIN
-# ===============================
+# ==========================
+# LOGIN PROFISSIONAL
+# ==========================
 from auth.login import check_login
 from auth.logout import botao_logout
 
 check_login()
 
-# ===============================
+# ==========================
 # INTERFACE
-# ===============================
+# ==========================
 st.title("⚙️ Moto-Renow")
 
 botao_logout()
 
-# ===============================
-# MENU
-# ===============================
+# ==========================
+# MENU LATERAL
+# ==========================
 menu = st.sidebar.radio(
-    "Menu",
+    "menu",
     [
-        "Cadastro",
-        "Consulta",
-        "Calculadora",
-        "Rebobinador"
+        "cadastro",
+        "consulta",
+        "calculadora",
+        "rebobinador"
     ]
 )
 
-# ===============================
-# CARREGAR PÁGINA
-# ===============================
-def carregar_pagina(modulo, nome):
+# ==========================
+# CARREGAMENTO DINÂMICO
+# ==========================
+def carregar_pagina(nome):
 
     try:
-        page = importlib.import_module(modulo)
+        modulo = f"page.{nome}"
+        pagina = importlib.import_module(modulo)
 
-        if hasattr(page, "show"):
-            page.show()
+        if hasattr(pagina, "show"):
+            pagina.show()
         else:
-            st.error(f"A página '{nome}' precisa ter função show()")
+            st.error(f"{nome}.py precisa da função show()")
 
     except ModuleNotFoundError:
-        st.error(f"Página '{nome}' não encontrada.")
+        st.error(f"Página '{nome}' não encontrada")
 
     except Exception as e:
-        st.error(f"Erro ao carregar {nome}")
         st.exception(e)
 
-# ===============================
-# ROTEAMENTO
-# ===============================
-paginas = {
-    "Cadastro": "page.cadastro",
-    "Consulta": "page.consulta",
-    "Calculadora": "page.calculadora",
-    "Rebobinador": "page.rebobinador",
-}
-
-carregar_pagina(paginas[menu], menu)
+carregar_pagina(menu)
