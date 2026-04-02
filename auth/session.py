c88cc039220e1b610f5308b5daac2be0a7f42d43
Motorespka <k45430494@gmail.com>
2026-04-01 21:55:00 -0300

    Update session.py

--- auth/session.py
+++ auth/session.py
@@ -3,31 +3,22 @@ from datetime import datetime, timedelta
 
 TEMPO_SESSAO = 8  # horas
 
-
 def criar_sessao():
     st.session_state.logado = True
-    st.session_state.expira_em = (
-        datetime.now() + timedelta(hours=TEMPO_SESSAO)
-    )
-
+    st.session_state.expira_em = datetime.now() + timedelta(hours=TEMPO_SESSAO)
 
 def sessao_valida():
-
+    # Verifica se a sessão existe e não expirou
     if "logado" not in st.session_state:
         return False
-
     if not st.session_state.logado:
         return False
-
     if "expira_em" not in st.session_state:
         return False
-
     if datetime.now() > st.session_state.expira_em:
         limpar_sessao()
         return False
-
     return True
-
 
 def limpar_sessao():
     for k in list(st.session_state.keys()):
